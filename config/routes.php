688977385fc37f937da5132269ed3afcdbd40135
Kostantin Makarenko <kochanmujik@gmail.com>
2022-12-08 20:33:13 +0200

    Modified routes

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
+    'edit/([0-9]+)' => 'site/update/$1',
+    'delete/([0-9]+)' => 'site/delete/$1',
     'create' => 'site/create',
-    'edit' => 'site/update',
-    'delete' => 'site/delete',
     'truncate' => 'site/truncate',
 
     '' => 'site/index'
